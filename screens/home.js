ea35f820e82cffe0396ac17739cc77c83b12dede
Yerassyl Tleugazy <tleugazy98@gmail.com>
2020-11-15 16:02:08 +0600

    sddsds

--- screens/home.js
+++ screens/home.js
@@ -1,12 +1,22 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Text, Button, FlatList, TouchableOpacity} from "react-native";
+import {View, Text, FlatList, TouchableOpacity, Modal, StyleSheet, TouchableWithoutFeedback, Keyboard} from "react-native";
 import {globalStyles} from "../styles/global";
 import Card from "../shared/card";
+import {MaterialIcons} from '@expo/vector-icons';
+import ReviewForm from "./reviewForm";
 
 export default function Home({navigation}) {
+    const [modalOpen, setModalOpen] = useState(false)
     const pressHandler = () => {
         navigation.navigate('ReviewDetails');
     };
+    const addReview = (review) => {
+        review.key = Math.random().toString();
+        setReviews((currentReviews) => {
+            return [review, ...currentReviews]
+        });
+        setModalOpen(false);
+    }
     const [reviews, setReviews] = useState([
         {title: "title1", rating: 5, body: "body1", key: '1'},
         {title: "title2", rating: 3, body: "body2", key: '2'},
@@ -16,6 +26,25 @@ export default function Home({navigation}) {
     ]);
     return (
         <View style={globalStyles.container}>
+            <Modal visible={modalOpen} animationType='slide'>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.modalContent}>
+                        <MaterialIcons
+                            name="close"
+                            size={24}
+                            onPress={() => setModalOpen(false)}
+                            style={{...styles.modalToggle, ...styles.modalClose}}/>
+                        <ReviewForm addReview={addReview}/>
+                    </View>
+                </TouchableWithoutFeedback>
+
+            </Modal>
+            <MaterialIcons
+                name="add"
+                size={24}
+                onPress={() => setModalOpen(true)}
+                style={styles.modalToggle}
+            />
             <FlatList
                 data={reviews}
                 renderItem={({item}) => (
@@ -31,3 +60,22 @@ export default function Home({navigation}) {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    modalContent: {
+        flex: 1,
+
+    },
+    modalToggle: {
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: "#f2f2f2",
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center',
+    },
+    modalClose: {
+        marginTop: 20,
+        marginBottom: 0
+    }
+})
